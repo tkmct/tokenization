c2febe50d3200c8cb0de7ec4d01adfa1dba2749e
tkmct <1220t.takamichi@gmail.com>
2020-04-19 10:42:45 +0900

    add token sale test

---
+++ test/MyTokenSale.test.js
@@ -0,0 +1,30 @@
+const Token = artifacts.require('MyToken')
+const TokenSale = artifacts.require('MyTokenSale')
+
+const chai = require('./chaisetup')
+const BN = web3.utils.BN
+const expect = chai.expect
+
+contract("TokenSale", async (accounts) => {
+  const [initialHolder, recipient, anotherAccount] = accounts
+
+  it('should not holds any coins', async () => {
+    const instance = await Token.deployed()
+    return expect(instance.balanceOf.call(initialHolder)).to.eventually.be.a.bignumber.equal(new BN(0))
+  })
+
+  it('all coins should be in the tokensale smart contract', async () => {
+    const instance = await Token.deployed()
+    const balance = await instance.balanceOf.call(TokenSale.address)
+    const totalSupply = await instance.totalSupply.call()
+    return expect(balance).to.be.a.bignumber.equal(totalSupply)
+  })
+
+  it('should be possible to buy one token by simply send ether to the smart contract', async () => {
+    const tokenInstance = await Token.deployed()
+    const tokenSaleInstance = await TokenSale.deployed()
+    const balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient)
+    expect(tokenSaleInstance.sendTransaction({ from: recipient, value: web3.utils.toWei('1', 'wei') })).to.be.fulfilled
+    return expect(balanceBeforeAccount + 1).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient))
+  })
+})
--- test/Mytoken.test.js
+++ test/Mytoken.test.js
@@ -1,13 +1,7 @@
 const Token = artifacts.require('MyToken')
 
-const chai = require('chai')
+const chai = require('./chaisetup')
 const BN = web3.utils.BN
-const chaiBN = require('chai-bn')(BN)
-chai.use(chaiBN)
-
-const chaiAsPromise = require('chai-as-promised')
-chai.use(chaiAsPromise)
-
 const expect = chai.expect
 
 contract('Token Test', async accounts => {
@@ -22,7 +16,7 @@ contract('Token Test', async accounts => {
     const instance = myToken
     const totalSupply = await instance.totalSupply()
 
-    expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply)
+    return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply)
   })
 
   it('can send tokens from Account 1 to account 2', async () => {
@@ -32,13 +26,13 @@ contract('Token Test', async accounts => {
     expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply)
     expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled
     expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)))
-    expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
+    return expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
   })
 
   it('is not possible to send more tokens than account 1 has', async () => {
     const instance = myToken
     const balance = await instance.balanceOf(initialHolder)
     expect(instance.transfer(recipient, new BN(balance + 1))).to.eventually.be.rejected
-    expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balance)
+    return expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(balance)
   })
 })
